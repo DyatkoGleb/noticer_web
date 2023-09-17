3675bb9cf70cd59ad5cfb83d7dff76c823f38df8
DyatkoGleb <glebikon@yandex.ru>
2023-09-17 14:26:48 +0300

    Positioning NewNoteForm and blocks with notes & notifications

--- src/App.js
+++ src/App.js
@@ -5,19 +5,31 @@ import NoticeList from './components/NoticeList'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom-bootstrap.css'
+import styled from "styled-components";
+
+
+const ContentContainer = styled.div`
+  max-width: 1000px;
+  padding: 0 50px;
+  margin: 10px auto 0 auto;
+`;
 
 
 function App() {
-  return (
-    <div>
-      <NewNoteForm></NewNoteForm>
+    return (
+        <ContentContainer>
+            <NewNoteForm></NewNoteForm>
 
-        <div className="d-flex justify-content-center">
-          <NoteList></NoteList>
-          <NoticeList></NoticeList>
-        </div>
-    </div>
-  );
+            <div className="row">
+                  <div className="col-12 col-md-6">
+                        <NoteList></NoteList>
+                  </div>
+                  <div className="col-12 col-md-6">
+                        <NoticeList></NoticeList>
+                  </div>
+            </div>
+        </ContentContainer>
+    );
 }
 
 
--- src/components/NewNoteForm.jsx
+++ src/components/NewNoteForm.jsx
@@ -12,24 +12,22 @@ const NewNoteForm = () => {
     }
 
     return (
-        <div className="content">
-            <div className="d-flex justify-content-center align-items-center" id="form">
-                <input 
-					className="add-note-input"
-					id="input" 
-					type="text" 
-					placeholder="24.01.2023 11:11 Text.." 
-					autoFocus
-                ></input>
-                <input 
-					className="add-note-btn"
-					id="btn-add" 
-					type="button" 
-					value="Добавить"
-					onClick={addNewNote}
-                ></input>
-            </div>
-        </div>
+		<div className="d-flex justify-content-center align-items-center" id="form">
+			<input
+				className="add-note-input"
+				id="input"
+				type="text"
+				placeholder="24.01.2023 11:11 Text.."
+				autoFocus
+			></input>
+			<input
+				className="add-note-btn"
+				id="btn-add"
+				type="button"
+				value="Добавить"
+				onClick={addNewNote}
+			></input>
+		</div>
     )
 }
 
