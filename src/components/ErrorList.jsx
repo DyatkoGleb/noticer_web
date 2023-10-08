2229cf258d43006555dc6271204240d31748fa44
DyatkoGleb <glebikon@yandex.ru>
2023-10-08 14:07:10 +0300

    Refactor: fixed size error list block

--- src/components/ErrorList.jsx
+++ src/components/ErrorList.jsx
@@ -1,23 +1,38 @@
 import ErrorItem from './ErrorItem'
 import styled from 'styled-components'
+import {useEffect, useRef} from "react";
 
 
 const StyledErrorList = styled.div`
     position: absolute;
     bottom: 0;
     right: 0;
+    max-height: 320px;
+    overflow: auto;
+
+    &::-webkit-scrollbar {
+        width: 0;
+    }
 `
 
 
 const ErrorList = ({ errors, removeError }) => {
+    const errorListRef = useRef()
+
+    useEffect(() => {
+        if (errorListRef.current) {
+            errorListRef.current.scrollTop = errorListRef.current.scrollHeight;
+        }
+    }, [errors])
+
     if (!errors) {
-        return
+        return null
     }
 
     return (
-        <StyledErrorList>
+        <StyledErrorList id="error-list" ref={errorListRef}>
             {errors.map(error =>
-                <ErrorItem key={error.id} error={error} onClick={() => removeError(error.id)}/>
+                <ErrorItem key={error.id} error={error} onClick={() => removeError(error.id)} />
             )}
         </StyledErrorList>
     )
